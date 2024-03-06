1c11a0f75e8243b564945935114bede20e65b61e
kavya-Sa <101489492+kavya-Sa@users.noreply.github.com>
2024-03-06 16:09:28 +0530

    Update SQLInjection.java

--- src/main/java/servlet/SQLInjection.java
+++ src/main/java/servlet/SQLInjection.java
@@ -11,33 +11,33 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-//@WebServlet("/rough")
+@WebServlet("/rough")
 public class SQLInjection extends HttpServlet {
    protected void doGet(HttpServletRequest req, HttpServletResponse res){
-//        try {
-//            Connection con= DAO.getConnection();
-//            PrintWriter out=res.getWriter();
-//            String userId=req.getParameter("userId");
-//            String sql="SELECT * FROM public.invoice WHERE  customer_id="+userId;
-//            //out.println(sql);
-//            Statement prepare=con.createStatement();
-//
-//            ResultSet rs=prepare.executeQuery(sql);
-//            out.printf("%-20s %-20s","invoice_id","amount");
-//            out.println(" ");
-//            while(rs.next()){
-//                int invoiceId=rs.getInt("id");
-//                int amount=rs.getInt("amount");
-//
-//      out.printf("%-20s %-20s",invoiceId,amount);
-//                out.println(" ");
-//
-//            }
-//            rs.close();
-//            prepare.close();
-//            con.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+       try {
+           Connection con= DAO.getConnection();
+           PrintWriter out=res.getWriter();
+           String userId=req.getParameter("userId");
+           String sql="SELECT * FROM public.invoice WHERE  customer_id="+userId;
+           //out.println(sql);
+           Statement prepare=con.createStatement();
+
+           ResultSet rs=prepare.executeQuery(sql);
+           out.printf("%-20s %-20s","invoice_id","amount");
+           out.println(" ");
+           while(rs.next()){
+               int invoiceId=rs.getInt("id");
+               int amount=rs.getInt("amount");
+
+     out.printf("%-20s %-20s",invoiceId,amount);
+               out.println(" ");
+
+           }
+           rs.close();
+           prepare.close();
+           con.close();
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
     }
 }
